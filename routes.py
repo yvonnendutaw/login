cac558b9954d4dea12488026391c8eb45fad7ad9
yvonnendutaw <yvonita7@gmail.com>
2016-07-11 23:29:44 +0300

    login template

--- routes.py
+++ routes.py
@@ -26,7 +26,7 @@ def admin_login():
 def test():
     POST_USERNAME = "python"
     POST_PASSWORD = "python"
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind = engine)
     s = Session()
     query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
     result = query.first()
